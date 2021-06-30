053e74d57f3c6d98cad908f7579646bf08ed4587
nfs951168 <86299467+nfs951168@users.noreply.github.com>
2021-06-30 11:30:30 +0100

    Update mssql-storage.sql

--- mssql-storage.sql
+++ mssql-storage.sql
@@ -1,4 +1,6 @@
-Use OutsystemsDB;
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+--Gives information about storage file fize and freespace
+--------------------------------------------------------------------------------------------------------------------------------------------------------
 
 ;WITH src AS
 (
@@ -28,3 +30,30 @@ SELECT [Filegroup] = FG, FileID, LogicalName, [Path],
   [%]         = CONVERT(decimal(5,2), 100.0*(DriveFreeMB)/DriveSizeMB)
 FROM src
 ORDER BY FG, LogicalName;
+
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--Returns size and rows of each user table in database
+
+--sys.tables: list each user table in current SQL Server Database
+--sys.indexes: Contains a row per index or heap of a tabular object, such as a table, view, or table-valued function
+--sys.partitions: has the partitions of all tables and the number of rows
+--sys_allocation_units: Number of pages of each allocation unit
+--sys.schemas: identifies the schema of each table
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+select	T.Name as [table-name],
+		s.name as [schema],
+		p.rows,
+		SUM(au.total_pages) * 8 [total-space-kb],
+		CAST(ROUND(((SUM(au.total_pages) * 8) / 1024.00), 2) AS NUMERIC(36, 2)) AS [total-space-mb],
+		SUM(au.used_pages) * 8 AS [used-space-kb], 
+		CAST(ROUND(((SUM(au.used_pages) * 8) / 1024.00), 2) AS NUMERIC(36, 2)) AS [used-space-mb], 
+		(SUM(au.total_pages) - SUM(au.used_pages)) * 8 AS [unused-space-kb],
+		 CAST(ROUND(((SUM(au.total_pages) - SUM(au.used_pages)) * 8) / 1024.00, 2) AS NUMERIC(36, 2)) AS [unused-space-mb] 
+from	sys.tables T	inner join sys.indexes I on (i.object_id = t.object_id)
+						inner join sys.partitions P on (p.object_id = i.object_id and p.index_id = p.index_id)
+						inner join sys.allocation_units au on (au.container_id = p.partition_id)
+						left join sys.schemas s on (s.schema_id = t.schema_id)
+group by t.name, s.name, p.rows
