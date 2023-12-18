25892b872d842f5bff1c8fc20fb4340252b021a2
Nuno Fernandes <86299467+nfs951168@users.noreply.github.com>
2023-12-18 17:14:27 +0000

    Update performance.sql
    added cached query plans

--- performance.sql
+++ performance.sql
@@ -1,5 +1,5 @@
 -----------------------------------------------------------------------------------------------------
---Returns the most expensive queries in database
+--Returns the most expensive queries in database - query execution statistics
 -----------------------------------------------------------------------------------------------------
 SELECT	TOP 100 qt.Text,
 		qs.execution_count,
@@ -16,3 +16,20 @@ CROSS APPLY sys.dm_exec_sql_text(qs.sql_handle) qt
 CROSS APPLY sys.dm_exec_query_plan(qs.plan_handle) qp
 where	qs.creation_time >= '2023-12-12 00:00:00' -- all plans created after date
 ORDER BY qs.total_worker_time DESC -- CPU time
+
+
+-- ----------------------------------------------------------------
+-- Get current cached query plans
+-- ----------------------------------------------------------------
+SELECT 	db.name,
+  	cp.objtype AS ObjectType,
+  	OBJECT_NAME(st.objectid, st.dbid) AS ObjectName,
+  	cp.usecounts AS ExecutionCount,
+  	st.TEXT AS QueryText,
+  	qp.query_plan AS QueryPlan
+FROM 	sys.dm_exec_cached_plans AS cp
+  	CROSS APPLY sys.dm_exec_query_plan(cp.plan_handle) AS qp
+  	CROSS APPLY sys.dm_exec_sql_text(cp.plan_handle) AS st
+  	INNER JOIN sys.sysdatabases db on st.dbid = db.dbid
+where 	st.TEXT like '%%'
+and 	db.name = ''
